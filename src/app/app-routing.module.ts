889b7abea9818ad6ac54608a9153c3f71a90f661
Mikhail Tonkushin <tonkushin@mail.ru>
2023-03-22 15:35:58 +0300

    The infrastructure has been prepared for the test task implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Path } from '../shared/enums';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: Path.Form,
+    loadChildren: () => import('../modules/form/form.module').then(m => m.FormModule)
+  },
+  {
+    path: '',
+    redirectTo: Path.Form,
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpHeadersInterceptor } from '../shared/interceptors';
+import { API_PATH } from '../shared/tokens';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+const MAT_MODULES = [MatToolbarModule];
 
 @NgModule({
   declarations: [
@@ -10,9 +17,24 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    ...MAT_MODULES
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpHeadersInterceptor,
+      multi: true,
+    },
+    {
+      provide: API_PATH,
+      useValue: window.location.origin + '/api' // because of the proxy.config.json
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
